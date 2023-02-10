c414be85efd3e11ad0a054e3ef8d1dab949dacfe
Gjon Gojcevic <gjonn@hotmail.com>
2023-02-10 11:13:30 -0500

    initial

---
+++ todoapi/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using todoapi;
+
+namespace todoapi
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
